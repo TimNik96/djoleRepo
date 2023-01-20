dd7972a71f7dc851a3d7436b47a62a7ae2f15f2a
ntim <timotic.nikola.27@gmail.com>
2023-01-20 15:43:06 +0100

    start js retrospective

---
+++ retrospective.js
@@ -0,0 +1,27 @@
+// data types
+
+// primitive
+    // number
+    // string
+    // boolean
+    // symbol
+    // null
+    // undefined
+
+// complex
+    // object
+        // array
+        // function
+
+// let imePromenljive = vrednost
+// a-z A-Z 0-9 _ $
+// deklaracija promenljive - kreiranje promenljive u memoriji (inicijalno dobija vrednost undefined)
+// inicijalizacija promenljive - dodela vrednosti promenljivoj
+
+let mojBroj = 20
+let broj = 10
+// let broj2 = 20
+
+console.log(broj, mojBroj)
+
+
